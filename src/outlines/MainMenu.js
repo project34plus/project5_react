17beb07aefecbb008e3323934db49eaedb363194
soeunl <soeun318@gmail.com>
2024-09-19 13:57:41 +0900

    container 수정

--- src/outlines/MainMenu.js
+++ src/outlines/MainMenu.js
@@ -6,18 +6,13 @@ import { getCommonStates } from '@/commons/contexts/CommonContext';
 import styled from 'styled-components';
 import { useTranslation } from 'react-i18next';
 
-const { navy, white, gray } = color;
-
-const { small } = fontSize;
-
-const { bold } = fontWeight;
-
 const MenuContainer = styled.nav`
   background-color: ${({ theme }) => theme.color.white};
   border-bottom: 2px solid ${({ theme }) => theme.color.navy};
   position: relative;
   left: 0;
   width: 100%;
+  height: 100px;
   z-index: 1000;
   display: flex;
   justify-content: center;
@@ -31,27 +26,28 @@ const MenuList = styled.ul`
 
 const MenuItem = styled.li`
   margin: 10px;
-  width: 150px;
+  width: 180px;
   display: flex;
   justify-content: center;
   align-items: center;
-`;
 
-const MenuLink = styled.a`
-  display: inline-block;
-  padding: 10px 15px;
-  font-weight: ${({ theme }) => theme.fontWeight.bold};
-  font-size: ${({ theme }) => theme.fontSize.small};
-  transition: background-color 0.3s, color 0.3s, transform 0.3s, box-shadow 0.3s;
+  a {
+    display: inline-block;
+    padding: 10px 15px;
+    font-weight: ${({ theme }) => theme.fontWeight.bold};
+    font-size: ${({ theme }) => theme.fontSize.normal};
+    transition: background-color 0.3s, color 0.3s, transform 0.3s,
+      box-shadow 0.3s;
 
-  &:hover {
-    background-color: ${({ theme }) => theme.color.white};
-    color: ${({ theme }) => theme.color.navy};
-    transform: translateY(-2px);
-  }
+    &:hover {
+      background-color: ${({ theme }) => theme.color.white};
+      color: ${({ theme }) => theme.color.navy};
+      transform: translateY(-2px);
+    }
 
-  &:active {
-    transform: translateY(0);
+    &:active {
+      transform: translateY(0);
+    }
   }
 `;
 
@@ -64,34 +60,22 @@ const MainMenu = () => {
       <MenuContainer>
         <MenuList>
           <MenuItem>
-            <MenuLink>
-              <a href="/mypage/info">{t('논문학술자료')}</a>
-            </MenuLink>
+            <a href="/mypage/info">{t('논문학술자료')}</a>
           </MenuItem>
           <MenuItem>
-            <MenuLink>
-              <a href="/mypage/info">{t('논문_등록')}</a>
-            </MenuLink>
+            <a href="/mypage/info">{t('논문_등록')}</a>
           </MenuItem>
           <MenuItem>
-            <MenuLink>
-              <a href="/mypage/info">{t('최신_연구_트렌드')}</a>
-            </MenuLink>
+            <a href="/mypage/info">{t('최신_연구_트렌드')}</a>
           </MenuItem>
           <MenuItem>
-            <MenuLink>
-              <a href="/mypage/info">{t('알고리즘')}</a>
-            </MenuLink>
+            <a href="/mypage/info">{t('알고리즘')}</a>
           </MenuItem>
           <MenuItem>
-            <MenuLink>
-              <a href="/mypage/info">{t('연구노트')}</a>
-            </MenuLink>
+            <a href="/mypage/info">{t('연구노트')}</a>
           </MenuItem>
           <MenuItem>
-            <MenuLink>
-              <a href="/mypage/info">{t('이용안내')}</a>
-            </MenuLink>
+            <a href="/mypage/info">{t('이용안내')}</a>
           </MenuItem>
         </MenuList>
       </MenuContainer>
