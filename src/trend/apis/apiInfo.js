ad539a970dba18f0ca66b00c360369352827943f
hyeon12 <seojh912_@naver.com>
2024-09-23 14:35:54 +0900

    api요청

--- src/trend/apis/apiInfo.js
+++ src/trend/apis/apiInfo.js
@@ -31,3 +31,19 @@ export const apiFieldRanking = (search) => {
 
   return requestData(url);
 };
+
+// 직업그룹별 인기 키워드 통계
+export const apiGroupRanking = (search) => {
+  search = search ?? {};
+
+  const qs = [];
+
+  for (const [k, v] of Object.entries(search)) {
+    qs.push(`${k}=${v}`);
+  }
+
+  let url = '/thesis/trend/keyword';
+  if (qs.length > 0) url += `?${qs.join('&')}`;
+
+  return requestData(url);
+};
